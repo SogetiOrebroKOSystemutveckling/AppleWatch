f9d1ae1f32f383c607cd36963fd1424d12488a3a
monkeypro <johan.langeville@sogeti.se>
2015-11-04 22:22:32 +0100

    Player

---
+++ AppleWatch/AppleWatch/AppleWatch/Pages/MainTabbedPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using AppleWatch.ViewModels;
+using Xamarin.Forms;
+
+namespace AppleWatch.Pages
+{
+    public class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            Children.Add(
+                new PlayerPage
+                {
+                    BindingContext = new PlayerViewModel
+                    {
+                        Name = "Köket", 
+                        Song = new SongViewModel("Queen", "Champions"),
+                        NextSong = new SongViewModel("Queen", "Bohemian Rapsody")
+                    }
+                }
+            );
+
+            Children.Add(
+                new PlayerPage
+                {
+                    BindingContext = new PlayerViewModel
+                    {
+                        Name = "Vardagsrummet",
+                        Song = new SongViewModel("Pearl Jam", "Alive"),
+                        PreviousSong = new SongViewModel("Pearl Jam", "Even flow"),
+                        NextSong = new SongViewModel("Pearl Jam", "Just breathe")
+                    }
+                }
+            );
+        }
+    }
+}
---
+++ AppleWatch/AppleWatch/AppleWatch/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleWatch.ViewModels
+{
+    public class PlayerViewModel
+    {
+        public string Name { get; set; }
+
+        public SongViewModel Song { get; set; }
+
+        public SongViewModel NextSong { get; set; }
+        public SongViewModel PreviousSong { get; set; }
+    }
+
+    public class SongViewModel
+    {
+        public SongViewModel(string artist, string title)
+        {
+            Artist = artist;
+            Title = title;
+        }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+    }
+}
